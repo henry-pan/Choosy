ab6c44bd3fce6412a6c1d0861d70f2bea7fa72fb
natkozak <natkozak0@gmail.com>
2021-04-19 09:41:16 -0700

    Add boilerplate idea validation

---
+++ validation/ideas.js
@@ -0,0 +1,23 @@
+// validation/ideas.js
+
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateIdeaInput(data) {
+  let errors = {};
+
+  data.text = validText(data.text) ? data.text : '';
+
+  if (!Validator.isLength(data.text, { min: 5, max: 140 })) {
+    errors.text = 'Idea must be between 5 and 140 characters';
+  }
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = 'Text field is required';
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
+};
