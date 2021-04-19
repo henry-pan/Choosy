2899c444e86d767b5ce4254884c85e9ed97e32ba
Henry <henry-pan@users.noreply.github.com>
2021-04-19 01:12:16 -0700

    Initial commit for Choosy

---
+++ README.md
@@ -0,0 +1,7 @@
+# Choosy
+
+Have you ever been out with your friends and can't decide what to do or where to go? Do you have trouble deciding on which item to cross off of your to-do list first? Do you ever need a quick way to bounce your decisions off of something?
+
+Choosy is a mobile-friendly app that helps individuals and groups quickly make decisions. Users will collaboratively make a list of all the possible decisions, and then go through quick rounds of voting methods that narrow the more popular decisions down to a final winner.
+
+Choosy is built using the MERN stack, with the voting and logic on the frontend, and the user database and optional history of past decisions stored in the backend.
---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require('passport');
+const app = express();
+const db = require("./config/keys").mongoURI;
+
+// const path = require('path');
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('frontend/build'));
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   })
+// }
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+  
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
