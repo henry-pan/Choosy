4a1fc27f05d620abda39e91a089f4da774246f03
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-19 09:51:03 -0700

    Add route use in app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const passport = require('passport');
 const app = express();
 const db = require("./config/keys").mongoURI;
+const users = require('./routes/api/users');
+const ideas = require('./routes/api/ideas');
+
+
 
 // const path = require('path');
 // if (process.env.NODE_ENV === 'production') {
@@ -24,6 +28,12 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 app.use("/api/users", users);
+app.user("/api/ideas", ideas);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
