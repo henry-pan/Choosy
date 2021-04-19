6a86a54c4452ac2e1494ec156f3c93dda8daa476
Henry <henry-pan@users.noreply.github.com>
2021-04-19 10:09:20 -0700

    Add files for components

---
+++ frontend/src/components/splash/splash_page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SplashPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Choosy</h1>
+      </div>
+    );
+  }
+}
+
+export default SplashPage;
