ddaf6761226414885c7df3fb56b1d64bbd1b7bf2
Henry <henry-pan@users.noreply.github.com>
2021-04-20 01:55:24 -0700

    Add auth stylesheet

--- frontend/src/components/session/session_form.js
+++ frontend/src/components/session/session_form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import "./auth.css";
 
 class SessionForm extends React.Component {
   constructor(props) {
@@ -51,31 +52,29 @@ class SessionForm extends React.Component {
     let usernameInput;
     let password2Input;
     if (this.props.formType === "Register") {
-      usernameInput = <input onChange={this.handleInput("name")} type="text" value={this.state.name} placeholder="Your name"/>;
-      password2Input = <input onChange={this.handleInput("password2")} type="password" value={this.state.password2} placeholder="Confirm your password"/>;
+      usernameInput = <input className="auth-input" onChange={this.handleInput("name")} type="text" value={this.state.name} placeholder="Your name"/>;
+      password2Input = <input className="auth-input" onChange={this.handleInput("password2")} type="password" value={this.state.password2} placeholder="Confirm password"/>;
     }
     
     let demoLogin;
     if (this.props.formType === "Login") {
       demoLogin = <>
-        <div>
-          <button onClick={(e) => this.handleDemo("demo@demo.com", e)}>Demo Login</button>
-        </div>
+        <button className="link-btn auth-btn" onClick={(e) => this.handleDemo("demo@demo.com", e)}>Demo Login</button>
         <div className="divider"><p>or</p></div>
       </>;
     }
 
     return (
-      <div>
-        <h1>Choosy</h1>
+      <div className="auth-main">
+        <h1 className="auth-logo">choosy</h1>
         {demoLogin}
-        <form onSubmit={this.handleSubmit}>
+        <form className="auth-form" onSubmit={this.handleSubmit}>
           {usernameInput}
-          <input onChange={this.handleInput("email")} type="email" value={this.state.email} placeholder="Your email"/>
-          <input onChange={this.handleInput("password")} type="password" value={this.state.password} placeholder="Your password"/>
+          <input className="auth-input" onChange={this.handleInput("email")} type="email" value={this.state.email} placeholder="Your email"/>
+          <input className="auth-input" onChange={this.handleInput("password")} type="password" value={this.state.password} placeholder="Your password"/>
           {password2Input}
           {this.renderErrors()}
-          <button>{this.props.formType}</button>
+          <button className="link-btn auth-btn" >{this.props.formType}</button>
         </form>
       </div>
     );
