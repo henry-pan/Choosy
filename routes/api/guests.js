1dd898cf69305a15ee2748dabf1eb0e6c74a3e3c
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-21 14:39:37 -0700

    Fix fetch single guest

--- routes/api/guests.js
+++ routes/api/guests.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const Guest = require('../../models/Guest');
-const validateGuestName = require('../../validation/guests');
+const validateGuestNameInput = require('../../validation/guests');
+const { json } = require('body-parser');
+const mongoose = require("mongoose");
 
 router.get("/test", (req, res) => res.json({ msg: "This is the guests route" }));
 
 //fetch all guests
 router.get("/", (req, res) => {
+  debugger
   Guest
     .find()
     .then(guests => res.json(guests))
@@ -17,18 +20,17 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   Guest
     .findById(req.params.id)
-    .then(guest => guest.json(guest))
-    .catch(err => res.status(400).json(err));
+    .then(guest => res.json(guest))
+    .catch(err => res.status(400).json({ noguestfound: "This guest does not exist"}));
 });
-
 
 //create a guest
 router.post("/", (req, res) => {
-  const { errors, isValid } = validateGuestName(req.name);
+  // const { errors, isValid } = validateGuestNameInput(req.name);
 
-  if (!isValid) {
-    return res.status(json(errors));
-  }
+  // if (!isValid) {
+  //   return res.status(json(errors));
+  // }
 
   const newGuest = new Guest({
     name: req.body.name
@@ -39,6 +41,7 @@ router.post("/", (req, res) => {
     .then(guest => res.json(guest));
 });
 
+//delete a guest by id
 router.delete("/:id", (req, res) => {
   Guest
     .findByIdAndDelete(req.params.id)
--- validation/guests.js
+++ validation/guests.js
@@ -1,21 +1,21 @@
 const Validator = require('validator');
 const validText = require('./valid-text');
 
-module.exports = function validateGuestName(data) {
+module.exports = function validateGuestNameInput(data) {
   let errors = {};
 
   data.name = validText(data.name) ? data.name : '';
-
+  
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name is required";
   }
 
-  if (!Validator.isLength(data.body, { min: 2, max: 20 })) {
+  if (!Validator.isLength(data.name, { min: 2, max: 20 })) {
     errors.name = "Name must be between 2 and 20 characters";
   }
 
   return {
     errors,
     isValid: Object.keys(errors).length === 0
-  }
-}
+  };
+};
