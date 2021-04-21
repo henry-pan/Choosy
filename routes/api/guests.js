ce2a25f0a33372489e14dd55ddc2de565cf7efe4
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-21 10:40:09 -0700

    Fix post route in guests

--- routes/api/guests.js
+++ routes/api/guests.js
@@ -5,5 +5,45 @@ const validateGuestName = require('../../validation/guests');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the guests route" }));
 
+//fetch all guests
+router.get("/", (req, res) => {
+  Guest
+    .find()
+    .then(guests => res.json(guests))
+    .catch(err => res.status(400).json(err));
+});
+
+//fetch a single guest by id
+router.get("/:id", (req, res) => {
+  Guest
+    .findById(req.params.id)
+    .then(guest => guest.json(guest))
+    .catch(err => res.status(400).json(err));
+});
+
+
+//create a guest
+router.post("/", (req, res) => {
+  const { errors, isValid } = validateGuestName(req.name);
+
+  if (!isValid) {
+    return res.status(json(errors));
+  }
+
+  const newGuest = new Guest({
+    name: req.body.name
+  });
+
+  newGuest
+    .save()
+    .then(guest => res.json(guest));
+});
+
+router.delete("/:id", (req, res) => {
+  Guest
+    .findByIdAndDelete(req.params.id)
+    .then(guest => res.json(guest))
+    .catch(err => res.status(400).json(err));
+});
 
 module.exports = router;
