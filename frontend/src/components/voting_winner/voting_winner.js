d3424ac05895013592d0f552f975177a13181044
byoung <youngben@earthlink.net>
2021-04-23 12:04:21 -0700

    single user version works throughout

--- frontend/src/components/voting_winner/voting_winner.js
+++ frontend/src/components/voting_winner/voting_winner.js
@@ -19,7 +19,7 @@ class VotingWinner extends React.Component {
         </div>
         <div className="voting-winner-container">
           <h1 className="title voting-winner-title">Winner</h1>
-          <h1 className="voting-winner-item">{this.props.idea}</h1>
+          <h1 className="voting-winner-item">{this.props.idea.body}</h1>
           <p className="voting-winner-blurb">Liked this set of ideas?</p>
           <p className="voting-winner-blurb">{saveIdeasText}</p>
           <button className="link-btn">{this.props.loggedIn ? "Save" : "Register"}</button>
