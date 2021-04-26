8bb0315095c02a394133b3cf7e21768ad8965460
byoung <youngben@earthlink.net>
2021-04-25 21:17:42 -0700

    finished logic for algo, NOT IMPLEMENTED

--- frontend/src/components/room/room.js
+++ frontend/src/components/room/room.js
@@ -72,6 +72,34 @@ class Room extends React.Component{
     return winner;
   }
 
+  randomizeArr(inputArr) {
+    let arr = [...inputArr];
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i)
+      const temp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = temp 
+    }
+    return arr;
+  }
+
+  subDivideArrByScore(arr) {
+    let newArr = [...arr];
+    let randArr = [];
+    let startI = 0;
+    for (let i = 0; i < newArr.length; i++) {
+      if ((newArr[i].__v < newArr[i + 1].__v || newArr[i + 1] === undefined)) {
+        randArr = randArr.concat([newArr.slice(startI, i + 1)]);
+        startI = i + 1;
+      };
+    }
+    return randArr;
+  }
+
+  //separate sorted array into separate arrays by score
+  //randomize arrays
+  //concat arrays
+
   componentWillUnmount() {
     clearInterval(this.interval);
   }
