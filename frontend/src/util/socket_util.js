5f3657bffc6592641427ee31c48756f75fb043c1
natkozak <natkozak0@gmail.com>
2021-05-12 15:04:27 -0700

    Pre-load usernames

--- frontend/src/util/socket_util.js
+++ frontend/src/util/socket_util.js
@@ -1,7 +1,14 @@
 import io from "socket.io-client";
 
 export const socket = () => {
-  const socket = io();
+  // const socket = io();
+
+  let socketURL = "127.0.0.1:5000";
+  if (process.env.NODE_ENV === "production") {
+    socketURL =
+      process.env.REACT_APP_SOCKET_URL || "http://choosyapp.herokuapp.com/";
+  }
+  let socket = io(socketURL, { transports: ["websocket"] });
 
   // submit name form
   const form = document.getElementById('form-test');
@@ -18,12 +25,22 @@ export const socket = () => {
   //add username to list
   const usernames = document.getElementById('usernames');
 
+  socket.on('load usernames', (usernamesArr) => {
+    for (let i = 0; i < usernamesArr.length; i++) {
+      var item = document.createElement('li');
+      item.textContent = usernamesArr[i];
+      item.classList.add('room-user-item');
+      console.log(usernamesArr);
+      usernames.appendChild(item);
+    }
+  });
+
   socket.on('submit username', function (username) {
     var item = document.createElement('li');
     item.textContent = username;
     item.classList.add('room-user-item');
     usernames.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
+    // window.scrollTo(0, document.body.scrollHeight);
   });
 
 }
--- src/socket_draft.js
+++ src/socket_draft.js
@@ -1,10 +1,37 @@
 const io = require("../app").io;
 
+// NOTE: to debug, write on the frontend browser console: 
+// localStorage.debug = 'socket.io-client:socket'
 
-// emits the username
+
+let usernames = [];
+
 io
   .on('connection', (socket) => {
+
+    socket.emit('load usernames', usernames);
+
+    // if we saved the names in the room somehow, we may be able to use something like below to get them from the database, similar to an express route:
+    // Room.find({}, (err, usernames) => {
+    //   if (err) throw err;
+    //   console.log('sending previous messages');
+    //   socket.emit('load usernames', usernames);
+    // })
+    
+
+    // socket.on("connect_error", (err) => {
+    //   console.log(`connect_error due to ${err.message}`);
+    // });
+
+    // emits the username
     socket.on('submit username', (username) => {
       io.emit('submit username', username);
-    })
+      console.log(username);
+      console.log(usernames.concat(username));
+      usernames = usernames.concat(username);
+      console.log(usernames);
+    });
+
   });
+
+
